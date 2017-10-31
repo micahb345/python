bf540b471497af79593815cfe2f281da63349d89
micahb345 <31550876+micahb345@users.noreply.github.com>
2017-10-31 11:38:22 -0700

    Update random list.py

--- random list.py
+++ random list.py
@@ -1,28 +1,34 @@
 from random import randint
 def randlist(r,usedlist,done):
 	sum = 0
-	alpha = [ "A","b","B","c","C","d","G","H","I","J",
-		"J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","and","Z"]
+	alpha = [ "A","B","C","D","E","F","G","H","I",
+				"J","K","L","M","N","O","P","Q","R","S",
+				"T","U","V","W","X","Y","and","Z","#",
+				"={}","{","|","*","!","","","","","","","","",""
+				"","","","","","","","","","","","","","","^","_","`","a","b","c","d","e" "F"
+	         "g","h",'i','j','k','l','m','n','o','p',
+	         'q','r','s','t','u','v','w','x','y','z',
+	         '{','|','}','~','-','&','$','@','>','(','+','?']
 	usedlist[r] = 1
 	c = alpha[r]
+	print (len(usedlist))
 	
 	for i in range(len(usedlist)):
 		sum = sum + usedlist[i]
-	#print (c,usedlist,' sum ',sum)
+	#print (c,usedlist," sum ",sum)
 	if sum == 6:
 		done = True
 	return c,usedlist,done 
 	
 def main():
 	# initial variables
-	used = [,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	used = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
 	done = False #boolean
 	#####################
 	while done == False:
-		r = randint(0,5)
+		r = randint(0,94)
 		c,used,done = randlist(r,used,done)
-		c,used,done = randlist(r,used,done)
-		print("\033[1;32;40m i did it mr. coleman  \n")
-		#print (used)
-		print(c,end="")
+		print (used,r,c,done)
+		#print(c,end="")
 main()
+
